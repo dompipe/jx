f7c8113101c130854072e0f252f2047c2601ccc2
dompipe <inland14@live.com>
2026-08-22 13:38:41 -0400

    PASL: front p1 + back payload parts

--- pasl/pasl-back.php
+++ pasl/pasl-back.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-$_p = __DIR__ . '/.pasl-back.payload.body.php';
+$_p = __DIR__ . '/.pasl-back.body.php';
 if (!is_file($_p)) {
     require_once __DIR__ . '/pasl-back.payload.php';
     file_put_contents($_p, pasl_back_body());
--- pasl/pasl-front.php
+++ pasl/pasl-front.php
@@ -1,5 +1,5 @@
 <?php declare(strict_types=1);
-$_p = __DIR__ . '/.pasl-front.payload.body.php';
+$_p = __DIR__ . '/.pasl-front.body.php';
 if (!is_file($_p)) {
     require_once __DIR__ . '/pasl-front.payload.php';
     file_put_contents($_p, pasl_front_body());
