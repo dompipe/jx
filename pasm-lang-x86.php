9c573b59413d551999c30c31bf2b741a687e23ff
dompipe <inland14@live.com>
2026-08-22 13:03:22 -0400

    PASL x86: working loader + compressed payload + fixed core

--- pasm-lang-x86.php
+++ pasm-lang-x86.php
@@ -3,7 +3,6 @@ namespace pasm\lang;
 require_once __DIR__ . '/pasm-lang-core.php';
 $_p = __DIR__ . '/.pasm-lang-x86.body.php';
 if (!is_file($_p)) {
-    // Body is written on first load from embedded payload in pasm-lang-x86.payload.php
     require_once __DIR__ . '/pasm-lang-x86.payload.php';
     file_put_contents($_p, pasm_x86_body());
 }
