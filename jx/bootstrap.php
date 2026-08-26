6c41b9e61d5129082acfde44e40058450fdbf5b3
dompipe <inland14@live.com>
2026-08-26 12:27:49 -0400

    feat(bootstrap): load binding coercion

--- jx/bootstrap.php
+++ jx/bootstrap.php
@@ -10,6 +10,7 @@
 require_once dirname(__DIR__) . '/jx.php';
 require_once __DIR__ . '/Flow.php';
 require_once __DIR__ . '/SQL.php';
+require_once __DIR__ . '/BindingCoercion.php';
 
 $control = dirname(__DIR__) . '/pasl/xi/src/Control.php';
 if (is_file($control)) require_once $control;
